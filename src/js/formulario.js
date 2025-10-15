0c95211370e1a3a19782fe0bbc70387040f633d9
Tiago Silva <tiagosilva_live_2016@outlook.com>
2025-10-15 19:09:28 -0300

    refatoração na validação de formularioValido

--- src/js/formulario.js
+++ src/js/formulario.js
@@ -1,26 +1,24 @@
-const campoPreenchido = document.getElementsByName("inputs")
-const form = document.getElementById('formulario')
+const campos = document.getElementsByName("inputs");
+const form = document.getElementById('formulario');
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    const nome = campoPreenchido[0].value || "Usuario(a)"
-    const email = campoPreenchido[1].value
-    const fone = campoPreenchido[2].value
-    let formularioValido = true
+    const nome = campos[0].value || "Usuario(a)";
+    let formularioValido = true;
 
-    campoPreenchido.forEach((campo) => {
+    campos.forEach((campo) => {
         const preenchido = campo.value.trim() !== "";
 
         campo.classList.toggle('verde', preenchido);
         campo.classList.toggle("vermelho", !preenchido);
         campo.nextElementSibling.style.display = preenchido ? 'none' : 'block';
-        if(!preenchido) formularioValido = false;
+        
+        if (!preenchido) formularioValido = false;
     })
-    if (formularioValido && nome && email && fone) {
-        alert(`Parabéns ${nome} seu formulário foi enviado com sucesso!`)
-    } else {
-        alert(`${nome} todos os campos devem ser preenchidos para validação do formulário!`)
-    }
 
-})
+    formularioValido ?
+        alert(`Parabéns ${nome} seu formulário foi enviado com sucesso!`)
+        :
+        alert(`${nome} todos os campos devem ser preenchidos para validação do formulário!`)
+});
